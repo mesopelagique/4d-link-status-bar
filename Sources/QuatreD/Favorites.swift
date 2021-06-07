52017ad97e5d39435683593cd41e38a735d26680
eric.marchand <eric.marchand@4d.com>
2021-06-07 11:00:02 +0200

    Sort menu entry by modification date

--- Sources/QuatreD/Favorites.swift
+++ Sources/QuatreD/Favorites.swift
@@ -2,6 +2,8 @@ import Foundation
 import Combine
 
 public struct Favorites {
+    public static var pathExtension = "4DLink"
+    public static var favoritePrefix = "Favorites "
     public typealias Value = [String: [URL]]
     public static func get() -> AnyPublisher<Value, Never> {
         // return Just<[URL]>([]).eraseToAnyPublisher() // mock nothing
@@ -15,19 +17,34 @@ public struct Favorites {
                 promise(.success(urls))
                 return
             }
-            for url in folders.filter ({ $0.lastPathComponent.starts(with: "Favorites ")}) {
-                let version = url.lastPathComponent.replacingOccurrences(of: "Favorites ", with: "")
+            for url in folders.filter ({ $0.lastPathComponent.starts(with: favoritePrefix)}) {
+                let version = url.lastPathComponent.replacingOccurrences(of: favoritePrefix, with: "") // XXX clean only first or do substring
                 urls[version] = []
+                // XXX maybe submenu local and remote
                 if let files = try? FileManager.default.contentsOfDirectory(at: url.appendingPathComponent("Local"), includingPropertiesForKeys: [.isRegularFileKey], options: []) {
-                    urls[version]?.append(contentsOf: files.filter { $0.pathExtension == "4DLink" })
+                    urls[version]?.append(contentsOf: files.filter { $0.pathExtension == pathExtension }.sorted(by: { $0.contentModificationDate > $1.contentModificationDate}))
                 }
                 if let files = try? FileManager.default.contentsOfDirectory(at: url.appendingPathComponent("Remote"), includingPropertiesForKeys: [.isRegularFileKey], options: []) {
-                    urls[version]?.append(contentsOf: files.filter { $0.pathExtension == "4DLink" })
+                    urls[version]?.append(contentsOf: files.filter { $0.pathExtension == pathExtension }.sorted(by: { $0.contentModificationDate > $1.contentModificationDate}))
                 }
             }
             promise(.success(urls))
         }.eraseToAnyPublisher()
 
 
+    }
+}
+
+extension URL {
+
+    var contentModificationDate: Date {
+        assert(isFileURL)
+        let fileUrl = self
+        do {
+            return try fileUrl.resourceValues(forKeys: Set([URLResourceKey.contentModificationDateKey])).contentModificationDate ?? Date()
+        } catch let error as NSError {
+            print("\(#function) Error: \(error)")
+            return Date()
+        }
     }
 }
